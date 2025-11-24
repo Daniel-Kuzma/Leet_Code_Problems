4bd729773c6e170c644ae5f31f78dfaaec7cabe4
Daniel Kuźma <danielkuzma905@gmail.com>
2025-11-24 23:37:17 +0100

    Time: 286 ms (77.74%), Space: 67.4 MB (63.97%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
-    users = users[ ((users["mail"].str.match(r"^[a-z]")) | (users["mail"].str.match(r"^[A-Z]"))) & (users["mail"].str.endswith("@leetcode.com")) & (users["mail"].str.count(r"[^\w\@.-]") == 0) & (users["mail"].str.count("@") == 1)]
+    #users = users[ ((users["mail"].str.match(r"^[a-z]")) | (users["mail"].str.match(r"^[A-Z]"))) & (users["mail"].str.endswith("@leetcode.com")) & (users["mail"].str.count(r"[^\w\@.-]") == 0) & (users["mail"].str.count("@") == 1)]
+    users = users[users["mail"].str.match(r'^[a-zA-Z][\w.-]*@leetcode\.com$')]
     return users
 
 
