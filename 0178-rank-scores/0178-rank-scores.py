79a36f2d700655d99578fc6ccd3165dd3585fc06
Daniel Kuźma <danielkuzma905@gmail.com>
2025-11-28 22:52:27 +0100

    Time: 341 ms (5.14%), Space: 68 MB (79.18%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.py
+++ 0178-rank-scores/0178-rank-scores.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+"""def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
     scores["rank"] = None
     if scores["score"].count() == 0: return scores[["score", "rank"]]
     
@@ -19,5 +19,29 @@ def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
             max_rank = scores["score"].iloc[x]
             scores["rank"].iloc[x] = pom
 
-    return scores[["score", "rank"]]
+    return scores[["score", "rank"]]"""
+
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores["rank"] = None
+    if scores["score"].count() == 0: return scores[["score", "rank"]]
     
+    scores = scores["score"].sort_values(ascending = False).reset_index()
+    
+    pom = 1
+    max_rank = scores["score"].iloc[0]
+    
+    def funkcja_rankingu(wiersz):
+        nonlocal pom
+        nonlocal max_rank
+
+        if wiersz["score"] == max_rank:
+            return pom  
+        else: 
+            pom += 1
+            max_rank = wiersz["score"]
+            return pom
+
+    scores["rank"] = scores.apply(funkcja_rankingu, axis = 1)
+
+    return scores[["score", "rank"]]
